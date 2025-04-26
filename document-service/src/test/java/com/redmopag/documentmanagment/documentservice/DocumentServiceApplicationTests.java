ceb6638058c5c335c4c81958ab5d313da6883e56
redmopag <redmopag@gmail.com>
2025-04-26 18:53:28 +0500

    Create document-service

---
+++ document-service/src/test/java/com/redmopag/documentmanagment/documentservice/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmopag.documentmanagment.documentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
