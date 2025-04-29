e837c69d35a803d6a920923c749bf4fc9ca34168
redmopag <redmopag@gmail.com>
2025-04-29 17:50:43 +0500

    Init ocr service

---
+++ ocr-service/src/test/java/com/redmopag/documentmanagment/ocrservice/OcrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmopag.documentmanagment.ocrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
