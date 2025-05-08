a6fc3ba48a4b8a649f7bf249ea34f2c2fdaf537d
redmopag <redmopag@gmail.com>
2025-05-08 11:42:42 +0500

    Create text-service

---
+++ text-service/src/main/java/com/redmopag/documentmanagment/textservice/TextServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redmopag.documentmanagment.textservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextServiceApplication.class, args);
+    }
+
+}
---
+++ text-service/src/test/java/com/redmopag/documentmanagment/textservice/TextServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmopag.documentmanagment.textservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
